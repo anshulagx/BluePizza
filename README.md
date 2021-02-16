0209251fe6a4b48ce9cd8656daeeaafda7b85714
Kushagra Vaish <vaish.kushagra@gmail.com>
2021-02-17 00:15:43 +0530

    feat: Added initial version

---
+++ README.md
@@ -0,0 +1,89 @@
+<!-- PROJECT LOGO -->
+<p align="center">
+  <a href="https://dyte.in">
+    <img src="https://dyte-uploads.s3.ap-south-1.amazonaws.com/dyte-logo-dark.svg" alt="Logo" width="80">
+  </a>
+
+  <h3 align="center">Web integration Example</h3>
+
+  <p align="center">
+    A simple express app which integrates with dyte using REST APIs.
+    <br />
+    <a href="https://docs.dyte.in"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="https://app.dyte.in">View Demo</a>
+    ·
+    <a href="https://github.com/dyte-in/web-integration-example/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/dyte-in/web-integration-example/issues">Request Feature</a>
+  </p>
+</p>
+
+
+
+
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+* [About the Project](#about-the-project)
+  * [Built With](#built-with)
+* [Getting Started](#getting-started)
+  * [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+A basic express js project demonstrating how you can integrate your website with dyte.
+![very-basic](docs/site.png)
+
+It creates a new dyte meeting and then allows to join it as a participant or a host.
+
+### Built With
+
+* [express-generator](https://expressjs.com/en/starter/generator.html)
+* [dyte-client](https://www.npmjs.com/package/dyte-client)
+* [Pure CSS](https://purecss.io/)
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+Please make sure you have an organization ID and API Key for your application. These can be obtained from the developer portal.
+
+### Installation
+ 
+1. Clone the repo
+```sh
+git clone https://github.com/dyte-in/web-integration-example.git
+```
+2. Install NPM packages
+```sh
+npm install
+```
+3. Create an `.env` file with your credentials. Use `.env.example` as a template.
+```sh
+cp .env.example .env
+nano .env
+```
+4. Run the application
+```sh
+npm start
+```
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+You can use this example as a reference on how you can integrate your webapp with dyte.
+
+_For documentation on APIs and client SDKs, please refer to our [docs page](https://docs.dyte.in)._
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See [`LICENSE`](./LICENSE) for more information.
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,95 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
 
-/* GET home page. */
-router.get('/teacher', function(req, res, next) {
-  res.render('dyte-page', {type: "teacher", roomName: 'field-satellite', authToken: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQxYzhjMTI1LTQ0NGUtNDM5Zi05YzI5LTAyMThkZTFjZWY3OCIsImxvZ2dlZEluIjp0cnVlLCJpYXQiOjE2MDc0MzQxMjUsImV4cCI6MTYwNzUyMDUyNX0.WA0kNwnjVoXRbFHJBIGZXaaHYz5cSOPLvwZ6pn_MHdOuO6IyqAh2_MHL43qtM2VLGMSyxG68LWD_rh_h2mz7RlS3N7Y197MIFd_1Qc2if89fMkOaThm4-jI4mPepKhffSCoSiSaJT58ekgjn82w2kO77VUOSc2Wom-CziKB3fovVHzU0H9l7pxkGGQselY4nm81B-p0KJPL4vDJXuDBDSmJSleeaQMw0T_lszI4z8C8W5J3pfU3yLp_dObz3klrlsh0DruUrEYYTdUGHtQ5jiF86l8XDp0ZM5Xamv8OGAJ0KBYHRToE84cLx84q8Fp_pSqtSyDwc3c5KfhmNbVw4yA"});
-});
+const orgId = process.env.DYTE_ORG_ID;
+const apiKey = process.env.DYTE_API_KEY;
+const baseURL = process.env.DYTE_API_BASE_URL;
 
-router.get('/student', function(req, res, next) {
-  res.render('dyte-page', {type: "student", roomName: 'field-satellite', authToken: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQxYzhjMTI1LTQ0NGUtNDM5Zi05YzI5LTAyMThkZTFjZWY3OCIsImxvZ2dlZEluIjp0cnVlLCJpYXQiOjE2MDc0MzQxMjUsImV4cCI6MTYwNzUyMDUyNX0.WA0kNwnjVoXRbFHJBIGZXaaHYz5cSOPLvwZ6pn_MHdOuO6IyqAh2_MHL43qtM2VLGMSyxG68LWD_rh_h2mz7RlS3N7Y197MIFd_1Qc2if89fMkOaThm4-jI4mPepKhffSCoSiSaJT58ekgjn82w2kO77VUOSc2Wom-CziKB3fovVHzU0H9l7pxkGGQselY4nm81B-p0KJPL4vDJXuDBDSmJSleeaQMw0T_lszI4z8C8W5J3pfU3yLp_dObz3klrlsh0DruUrEYYTdUGHtQ5jiF86l8XDp0ZM5Xamv8OGAJ0KBYHRToE84cLx84q8Fp_pSqtSyDwc3c5KfhmNbVw4yA"});
-});
+if(!orgId || !apiKey || !baseURL) {
+  console.error("Define org id, API key and base URL to run this app.\nGet one from https://dev.dyte.in/");
+  process.exit(0);
+}
 
-router.get('/', function(req, res, next) {
+/**
+ * This exmaples creates a new meeting and then allow you to join that meeting as host participant.
+ * 1. User makes a POST call to create a new meeting
+ * 2. User can make `/host` and `/participant` GET calls to join the meeting
+ */
+
+
+router.get('/', function (req, res, next) {
   res.render('index');
 });
+
+router.post('/meeting', async function (req, res, next) {
+  const title = req.body.title;
+
+  // 1. Create a meeting with the given title
+   const resp = await axios.post(`${baseURL}/v1/organizations/${orgId}/meeting`, {
+    title: title || "New test meeting",
+  }, {
+    headers: {
+      'Authorization': `APIKEY ${apiKey}`
+    }
+  });
+
+  //2. Pass the relevant details to dahboard view
+  const meeting = resp.data.data.meeting;
+  const roomName = meeting.roomName;
+  const meetingId = meeting.id;
+  res.render('dashboard', { meetingId: meetingId, roomName });
+});
+
+router.get('/participant', async function (req, res, next) {
+  //1. Get the meeting ID from the request
+  const meetingId = req.query.meetingId;
+  const roomName = req.query.roomName;
+
+  //2. Make a call to create a participant
+  const resp = await axios.post(`${baseURL}/v1/organizations/${orgId}/meetings/${meetingId}/participant`, {
+    clientSpecificId: Math.random().toString(36).substring(7),
+    userDetails: {
+      "name": "Participant" + Math.random().toString(36).substring(2)
+    },
+  }, {
+    headers: {
+      'Authorization': `APIKEY ${apiKey}`
+    }
+  });
+
+  //3. Pass the relevant details to the page with client SDK
+  const authResponse = resp.data.data.authResponse;
+  const authToken = authResponse.authToken;
+  res.render('dyte-page', { type: "teacher", roomName , authToken, orgId, baseURL });
+});
+
+
+router.get('/host', async function (req, res, next) {
+  //1. Get the meeting ID from the request
+  const meetingId = req.query.meetingId;
+  const roomName = req.query.roomName;
+
+  //2. Make a call to create a participant with role `host`.
+  //   `host` and `participant` roles are added by default to org.
+  const resp = await axios.post(`${baseURL}/v1/organizations/${orgId}/meetings/${meetingId}/participant`, {
+    clientSpecificId: Math.random().toString(36).substring(7),
+    userDetails: {
+      "name": "Host" + Math.random().toString(36).substring(2)
+    },
+    roleName: "host"
+  }, {
+    headers: {
+      'Authorization': `APIKEY ${apiKey}`
+    }
+  });
+
+  //3. Pass the relevant details to the page with client SDK
+  const authResponse = resp.data.data.authResponse;
+  const authToken = authResponse.authToken;
+  res.render('dyte-page', { type: "teacher", roomName , authToken, orgId, baseURL });
+});
+
+
 
 module.exports = router;
